3b7b11c0e05875e71d4994e8bdcf47139a5fda96
Tudorin Danut <dan.tudorin@stud.acs.upb.ro>
2020-03-20 21:33:20 +0200

    adding ccs6814

--- arduino_ccs6814/arduino_ccs6814.ino
+++ arduino_ccs6814/arduino_ccs6814.ino
@@ -1,24 +1,250 @@
+#include <math.h>
+#include <Arduino.h>
 
-#define CO_port  A2
-#define NH3_port A1
-#define NO2_port A0
+enum channel {
+  CH_NH3, CH_RED, CH_OX
+};
+typedef enum channel channel_t;
+
+enum gas {
+  CO, NO2, NH3, C3H8, C4H10, CH4, H2, C2H5OH
+};
+typedef enum gas gas_t;
+
+#define NH3PIN A1
+#define COPIN A0
+#define OXPIN A2
+
+uint16_t NH3baseR;
+uint16_t REDbaseR;
+uint16_t OXbaseR;
+
+uint16_t getResistance(channel_t channel) {
+      unsigned long rs = 0;
+      int counter = 0;
+
+  switch (channel) {
+    case CH_NH3:
+      for(int i = 0; i < 100; i++) {
+        rs += analogRead(NH3PIN);
+        counter++;
+        delay(2);
+      }
+      return rs/counter;
+    case CH_RED:
+      for(int i = 0; i < 100; i++) {
+        rs += analogRead(COPIN);
+        counter++;
+        delay(2);
+      }
+      return rs/counter;
+    case CH_OX:      
+      for(int i = 0; i < 100; i++) {
+        rs += analogRead(OXPIN);
+        counter++;
+        delay(2);
+      }
+      return rs/counter;
+
+  }
+
+  return 0;
+}
+
+void calibrateMICS() {
+ 
+  uint8_t seconds = 10;
+  
+  uint8_t delta = 2;
+
+  uint16_t bufferNH3[seconds];
+  uint16_t bufferRED[seconds];
+  uint16_t bufferOX[seconds];
+  
+  uint8_t pntrNH3 = 0;
+  uint8_t pntrRED = 0;
+  uint8_t pntrOX = 0;
+  
+  uint16_t fltSumNH3 = 0;
+  uint16_t fltSumRED = 0;
+  uint16_t fltSumOX = 0;
+
+  uint16_t curNH3;
+  uint16_t curRED;
+  uint16_t curOX;
+
+  bool NH3stable = false;
+  bool REDstable = false;
+  bool OXstable = false;
+
+  for (int i = 0; i < seconds; ++i) {
+    bufferNH3[i] = 0;
+    bufferRED[i] = 0;
+    bufferOX[i] = 0;
+  }
+
+  do {
+    delay(1000);
+    unsigned long rs = 0;
+    delay(50);
+    for (int i = 0; i < 3; i++) {
+    delay(1);
+    rs += analogRead(NH3PIN);
+    }
+    curNH3 = rs/3;
+    rs = 0;
+    delay(50);
+    for (int i = 0; i < 3; i++) {
+    delay(1);
+    rs += analogRead(COPIN);
+    }
+    curRED = rs/3;
+    rs = 0;
+    delay(50);
+    for (int i = 0; i < 3; i++) {
+    delay(1);
+    rs += analogRead(OXPIN);
+    }
+    curOX = rs/3;
+
+    fltSumNH3 = fltSumNH3 + curNH3 - bufferNH3[pntrNH3];
+    fltSumRED = fltSumRED + curRED - bufferRED[pntrRED];
+    fltSumOX = fltSumOX + curOX - bufferOX[pntrOX];
+
+    bufferNH3[pntrNH3] = curNH3;
+    bufferRED[pntrRED] = curRED;
+    bufferOX[pntrOX] = curOX;
+    
+    NH3stable = abs(fltSumNH3 / seconds - curNH3) < delta;
+    REDstable = abs(fltSumRED / seconds - curRED) < delta;
+    OXstable = abs(fltSumOX / seconds - curOX) < delta;
+
+    pntrNH3 = (pntrNH3 + 1) % seconds ;
+    pntrRED = (pntrRED + 1) % seconds;
+    pntrOX = (pntrOX + 1) % seconds;
+    
+  } while (!NH3stable || !REDstable || !OXstable);
+
+  NH3baseR = fltSumNH3 / seconds;
+  REDbaseR = fltSumRED / seconds;
+  OXbaseR = fltSumOX / seconds;
+}
+
+uint16_t getBaseResistance(channel_t channel) {
+     switch (channel) {
+       case CH_NH3:
+         return NH3baseR;
+       case CH_RED:
+         return REDbaseR;
+       case CH_OX:
+         return OXbaseR;
+     } 
+  return 0;
+}
+
+float getCurrentRatio(channel_t channel) {
+  float baseResistance = (float) getBaseResistance(channel);
+  float resistance = (float) getResistance(channel);
+
+  return resistance / baseResistance * (1023.0 - baseResistance) / (1023.0 - resistance);
+  
+  return -1.0;
+}
+
+float measureMICS(gas_t gas) {
+  float ratio;
+  float c = 0;
+
+  switch (gas) {
+    case CO:
+      ratio = getCurrentRatio(CH_RED);
+      c = pow(ratio, -1.179) * 4.385;
+      break;
+    case NO2:
+      ratio = getCurrentRatio(CH_OX);
+      c = pow(ratio, 1.007) / 6.855;
+      break;
+    case NH3:
+      ratio = getCurrentRatio(CH_NH3);
+      c = pow(ratio, -1.67) / 1.47;
+      break;
+    case C3H8:
+      ratio = getCurrentRatio(CH_NH3);
+      c = pow(ratio, -2.518) * 570.164;
+      break;
+    case C4H10:
+      ratio = getCurrentRatio(CH_NH3);
+      c = pow(ratio, -2.138) * 398.107;
+      break;
+    case CH4:
+      ratio = getCurrentRatio(CH_RED);
+      c = pow(ratio, -4.363) * 630.957;
+      break;
+    case H2:
+      ratio = getCurrentRatio(CH_RED);
+      c = pow(ratio, -1.8) * 0.73;
+      break;
+    case C2H5OH:
+      ratio = getCurrentRatio(CH_RED);
+      c = pow(ratio, -1.552) * 1.622;
+      break;
+  }
+
+  return isnan(c) ? -1 : c;
+}
 
 void setup() {
-  Serial.begin(9600);
   
+  Serial.begin(9600);
+  calibrateMICS();
+  Serial.println("Calibration complete");
 }
 
 void loop() {
 
-      float CO_level = map(analogRead(CO_port), 0, 1023, 1, 1000);
-      float NH3_level = map(analogRead(NH3_port), 0, 1023, 1, 500);
-      float NO2_level = map(analogRead(NO2_port), 0, 1023, 0.05, 10);
-      
-      if(Serial.available() > 0) {
+  if(Serial.available() > 0) {
           int bufferedData = Serial.read();
-          String dataToSend = String(String(CO_level) + ";" + String(NH3_level) + ";" + String(NO2_level));
+          String dataToSend = String(String(measureMICS(CO)) + ";" + String(measureMICS(NH3)) + ";" + String(measureMICS(NO2)));
           
           Serial.println(dataToSend);
           
-        }      
+        }  
+ 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+//
+//
+//
+//
+//#define CO_port  A2
+//#define NH3_port A1
+//#define NO2_port A0
+//
+//void setup() {
+//  Serial.begin(9600);
+//  
+//}
+//
+//void loop() {
+//
+//      float CO_level = map(analogRead(CO_port), 0, 1023, 1, 1000);
+//      float NH3_level = map(analogRead(NH3_port), 0, 1023, 1, 500);
+//      float NO2_level = map(analogRead(NO2_port), 0, 1023, 0.05, 10);
+//      
+//          
+//}
--- esp32-recieveing-data-from-arduino/esp32-recieveing-data-from-arduino.ino
+++ esp32-recieveing-data-from-arduino/esp32-recieveing-data-from-arduino.ino
@@ -7,13 +7,13 @@
 #include <ThingSpeak.h>
 #include <WiFi.h>
 
-const char *ssid = "Ap6";
-const char *pwd = "ceparola?";
+const char *ssid = "FamTudorin";
+const char *pwd = "f988c124";
 const char * apiKey = "6BMM7OIENQ8EUPMU";
 const long channelNumber = 1013688;
 
 WiFiClient client;
-//HardwareSerial MySerial(1);
+HardwareSerial MySerial(1);
 HTU21D myHumidity;
 Adafruit_CCS811 ccs;
 SDS011 my_sds;
@@ -25,17 +25,19 @@ HardwareSerial port(2);
 float p10, p25;
 float CO2_level, VOC_level;
 int err;
-//String dataSent;
+String dataSent;
+float humd;
+float temp;
 
-//float CO_level;
-//float NO2_level;
-//float NH3_level;
+float CO_level;
+float NO2_level;
+float NH3_level;
 
 void setup() {
   
     Serial.begin(9600);
-    
-//    MySerial.begin(9600, SERIAL_8N1, 14, 12); //RX, TX
+    delay(3000);  
+    MySerial.begin(9600, SERIAL_8N1, 14, 12); //RX, TX
     port.begin(9600,SERIAL_8N1, 16, 17); //RX, TX
     my_sds.begin(&port);
     
@@ -48,9 +50,27 @@ void setup() {
       while(1);
     }
 
-    // Wait for the sensor to be ready
-    while(!ccs.available());
+    MySerial.write(0);
+    delay(1000);
+    if(MySerial.available() > 0) {
+         Serial.println("CJMCU 6814 is already calibrated");
+      }else {
+         Serial.print("CJMCU 6814 in calibration! please wait!");
+              while(MySerial.available() == 0) {
+                            Serial.print(".");
+                            delay(3000);
+              }
+        Serial.println(); 
+        String cjmcuMessage = MySerial.readString();
+        Serial.println(cjmcuMessage);
+        
+      }
 
+    // Wait for the sensor to be ready
+    while(!ccs.available()){
+       float temp = ccs.calculateTemperature();
+       ccs.setTempOffset(temp - 25.0);
+    }
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, pwd);
     
@@ -60,88 +80,93 @@ void setup() {
 }
 
 void loop() {
+   
+  MySerial.write(0);
+  delay(100);
+  if(MySerial.available() > 0) {
 
-  Serial.println("============START=============");
+       //  CJMCU 6814 -SENSOR
+       dataSent = MySerial.readString();
+       int index1 = dataSent.indexOf(';');
+       CO_level = dataSent.substring(0, index1).toFloat();
+       
+       int index2 = dataSent.indexOf(';', index1 + 1);
+       NH3_level = dataSent.substring(index1 + 1, index2).toFloat();
+       
+       int index3 = dataSent.indexOf(';', index2 + 1);
+       NO2_level = dataSent.substring(index2 + 1, index3).toFloat();
 
-////  CJMCU 6814 -SENSOR 
-//  MySerial.write(0);
-//  delay(100);
-//  if(MySerial.available() > 0) {
-//       
-//       dataSent = MySerial.readString();
-//       int index1 = dataSent.indexOf(';');
-//       CO_level = dataSent.substring(0, index1).toFloat();
-//       
-//       int index2 = dataSent.indexOf(';', index1 + 1);
-//       NH3_level = dataSent.substring(index1 + 1, index2).toFloat();
-//       
-//       int index3 = dataSent.indexOf(';', index2 + 1);
-//       NO2_level = dataSent.substring(index2 + 1, index3).toFloat();
-//
-//      Serial.print("CO_level : ");
-//      Serial.print(CO_level);
-//      Serial.print(" || NH3_level : ");
-//      Serial.print(NH3_level);
-//      Serial.print(" || NO2_level : ");
-//      Serial.println(NO2_level);
-//
-//      
-//    }
-//
-//   SDS011 sensor
+       Serial.print("CO_level : ");
+       Serial.print(CO_level);
+       Serial.print(" || NH3_level : ");
+       Serial.print(NH3_level);
+       Serial.print(" || NO2_level : ");
+       Serial.println(NO2_level);
+
+      //   SDS011 sensor
+
       err = my_sds.read(&p25, &p10);
       if (!err) {
         Serial.print("P2.5: " + String(p25) + "  ");
         Serial.println("P10:  " + String(p10));
       }
-    
- 
-//   Temperature and Humidity sensor
-  float humd = myHumidity.readHumidity();
-  float temp = myHumidity.readTemperature();
 
-  Serial.print("Temperature:");
-  Serial.print(temp, 1);
-  Serial.print("C");
-  Serial.print(" Humidity:");
-  Serial.print(humd, 1);
-  Serial.println("%");
+      //   Temperature and Humidity sensor
+      humd = myHumidity.readHumidity();
+      temp = myHumidity.readTemperature();
 
-//  CO2 -- VOC sensor
-  if(ccs.available()){
-    if(!ccs.readData()){
+      Serial.print("Temperature:");
+      Serial.print(temp, 1);
+      Serial.print("C");
+      Serial.print(" Humidity:");
+      Serial.print(humd, 1);
+      Serial.println("%");
+
+      //  CO2 -- VOC sensor
+      if(ccs.available()){
+        if(!ccs.readData()){
       
-      CO2_level = ccs.geteCO2();
-      Serial.print("CO2: ");
-      Serial.print(CO2_level);
-      Serial.print("ppm  || ");
-      
-      VOC_level = ccs.getTVOC();
-      Serial.print("TVOC: ");
-      Serial.print(VOC_level);
-      Serial.println(" ppb");
+          CO2_level = ccs.geteCO2();
+          Serial.print("CO2: ");
+          Serial.print(CO2_level);
+          Serial.print("ppm  || ");
+          
+          VOC_level = ccs.getTVOC();
+          Serial.print("TVOC: ");
+          Serial.print(VOC_level);
+          Serial.println(" ppb");
+        }
+        else{
+           Serial.println("ERROR!");
+           while(1);
+        }
     }
-    else{
-      Serial.println("ERROR!");
-      while(1);
-    }
-  }
+    Serial.println(uploadToThingSpeak(CO_level,NH3_level,NO2_level,temp,p25,p10,VOC_level,CO2_level));
+  } 
+}
+
+String uploadToThingSpeak(float CO_level, float NH3_level, float NO2_level,
+                              float temp, float p25, float p10, float VOC_level, float CO2_level) {
+           
     ThingSpeak.setField(1, temp);
-    ThingSpeak.setField(2, humd);
+    ThingSpeak.setField(2, CO_level);
     ThingSpeak.setField(3, p25);
     ThingSpeak.setField(4, p10);
     ThingSpeak.setField(5, CO2_level);
     ThingSpeak.setField(6, VOC_level);
+    ThingSpeak.setField(7, NO2_level);
+    ThingSpeak.setField(8, NH3_level);
     
-    int x = ThingSpeak.writeFields(channelNumber, apiKey);
-
-    if(x == 200) {
-        Serial.println("Channel updated successfuly");
-    }else {
-        Serial.println("Problem updating the channel");    
-    }
-    delay(20000); 
-    Serial.println("========================END===========================");      
+    ThingSpeak.writeFields(channelNumber, apiKey);
+    delay(20000);
+    
+    int response = ThingSpeak.getLastReadStatus();
+    
+    if(response == 200) {
+        return "successfully updated data to the server";
+      }else {
+        return "couldn't update data to the server";  
+      }                             
 }
 
 
